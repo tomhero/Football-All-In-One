9b6b365d70ae859fbf97bc01c765be0220ad8f0c
giffpong <s7070143@kmitl.ac.th>
2014-11-21 01:10:31 +0700

    Insert Footbal bg
    change normal photo to photo resize

--- project.py
+++ project.py
@@ -3,7 +3,6 @@
 """ ProJect """
 import time
 from Tkinter import *
-from ImageTk import PhotoImage 
 from PIL import Image, ImageTk
 import tkMessageBox
 import tkFont
@@ -18,7 +17,6 @@ class Program(object):
         app_name.place(x=20,y=0)
         image1 = Image.open("test.bmp")
         photo = ImageTk.PhotoImage(image1)
-        #test_photo = Label(self.root, image=photo).place(x=20, y=50)
         but = Button(self.root, image=photo, cursor="plus", command=self.football)
         but.image = photo
         but.place(x=25, y=50)
@@ -27,14 +25,17 @@ class Program(object):
         
     def football(self):
         self.ball = Toplevel()
-        self.ball.geometry('1366x768')
+        self.ball.geometry('%dx%d'% (1920, 1080))
+        self.ball.title('Football')
         label1 = Label(self.ball, text='Hello').place(x=50, y=50)
         bg_image = Image.open("fifa_15.jpg")
-        bg_photo =  ImageTk.PhotoImage(bg_image)
-        bg_pic = Label(self.ball, image=bg_photo, width=1366, height=768)
-        bg_pic.image = bg_photo
+        bg_image = bg_image.resize((1920,1080), Image.ANTIALIAS)
+        bg_photo = ImageTk.PhotoImage(bg_image)
+        bg_pic = Label(self.ball, image=bg_photo)
         bg_pic.place(x=0, y=0)
         self.ball.mainloop()
+
+    
 
 Program()
 
